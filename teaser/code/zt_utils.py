31350154608a5e9e73cba59d8390a528c7708a05
averyhugebug <wiktor.pronobis@gmail.com>
2015-03-08 05:53:09 +0100

    wiktor butt

--- teaser/code/zt_utils.py
+++ teaser/code/zt_utils.py
@@ -106,6 +106,29 @@ def compute_gate_prob(new_point, gate_point, sigma_gate, mean_gate):
     prob_of_new_point = gauss_pdf(dist, mean_gate, sigma_gate)
     return prob_of_new_point
 
+def wiktors_butt(new_point, sigma_spree):
+    X_coords = SPREE_X
+    Y_coords = SPREE_Y
+    nn_dist = float('inf')
+    nn = np.array([0., 0.])
+    for k in range(1, len(SPREE_X)):
+        p1 = np.array([X_coords[k-1], Y_coords[k-1]])
+        p2 = np.array([X_coords[k], Y_coords[k]])
+        m, y0 = compute_line(np.array([p1[0], p2[0]]), np.array([p1[1], p2[1]]))
+        pp = np.array(compute_nearest_point(new_point, m, y0))
+        if pp[0] < p1[0] or pp[0] > p2[0]:
+            p1_dist = np.linalg.norm(p1 - new_point)
+            p2_dist = np.linalg.norm(p2 - new_point)
+            if p1_dist < p2_dist:
+                pp = p1
+            else:
+                pp = p2
+        np_dist = np.linalg.norm(pp - nn)
+        if np_dist < nn_dist:
+            nn = pp
+            nn_dist = np_dist
+    prob_of_new_point = gauss_pdf(nn_dist, 0.0, sigma_spree)
+    return prob_of_new_point
 
 def compute_satt_prob(new_point, sigma_satt): 
     mean_satt = 0.
