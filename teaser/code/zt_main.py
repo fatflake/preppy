6aac207bbe94ecbf9970752bc5668017af50e8c1
Jackie <shelton@tu-berlin.de>
2015-03-26 13:54:24 +0100

    cleaned a bit, coord descent not done, but discretezation happy

--- teaser/code/zt_main.py
+++ teaser/code/zt_main.py
@@ -29,8 +29,10 @@ sattelite_x, sattelite_y = zt_utils.project_data(sattelite)
 # initial plot 
 zt_io.plot_everything([spree_x, spree_y], [gate_x, gate_y], [sattelite_x, sattelite_y]) 
 
+grad_ass_location = zt_utils.compute_grad_ass()
+print "Gradient descent location (not finished):", grad_ass_location
 # set resolution of 2D input grid
-RES = 500
+RES = 100
 
 # compute probabilities
 probs = zt_utils.compute_probs(RES) 
@@ -41,16 +43,22 @@ location_idx = np.unravel_index(probs.argmax(), (RES,RES))
 location_xy = zt_utils.index2xy(location_idx, RES)
 location_gps = zt_utils.gps_coords(location_xy)
 
+print "Resolution of discretization: %d x %d" % (RES, RES)
+
 sameh = np.array([[52.51104008, 13.45584084]])
 loc_sameh = zt_utils.project_data(sameh)
 gps_sameh = zt_utils.gps_coords(loc_sameh)
-print "Sameh's original GPS:", sameh
-print "Sameh's solution, XY coords:", loc_sameh, "GPS (recalc'ed) coords:", gps_sameh
-print "Our solution, index:", location_idx, "and XY coords:", location_xy, "and GPS:", location_gps
+print "Sameh:"
+print" -- original GPS:", sameh
+print "-- XY coords:", loc_sameh
+print "-- GPS (recalc'ed) coords:", gps_sameh
 
-print "Resolution of discretization: %d x %d" % (RES, RES)
-print "With probability ", max_prob, " that candidate is at GPS coordinates: ", location_gps
+print "Our solution, with -(log(probability)):", max_prob, ":"
+print "-- index:", location_idx
+print "-- XY coords:", location_xy
+print "-- GPS:", location_gps
 
+# TODO pygmaps
 # TODO fix plotting to use a coordinate mapping function
 
 # plot/save result
--- teaser/code/zt_utils.py
+++ teaser/code/zt_utils.py
@@ -6,6 +6,7 @@ from scipy.stats import lognorm
 import numpy as np
 import math
 from math import cos
+from scipy import optimize
 
 # io imports
 from zt_io import read_data, plot_individual
@@ -45,6 +46,7 @@ def project_data(data):
 
 SPREE_X, SPREE_Y = project_data(spree)
 GATE_X, GATE_Y = project_data(gate)
+GATE_X, GATE_Y = GATE_X[0], GATE_Y[0]
 SATT_X, SATT_Y = project_data(sattelite)
 
 
@@ -52,11 +54,17 @@ SATT_X, SATT_Y = project_data(sattelite)
 ############
 
 def index2xy(index, res):
+    """
+    From indices in the *res*x*res* space, project to (x,y) coordinates 
+    """
     p_x = X_MIN + (index[1] / float(res)) * (X_MAX - X_MIN)
     p_y = Y_MIN + (index[0] / float(res)) * (Y_MAX - Y_MIN)
     return [p_x, p_y]
 
 def gps_coords(points):
+    """
+    From orthogonally projected (x,y) points, project back to GPS coordinates
+    """
     p_lon = -( points[0] / (111.323*cos(SW_LAT)) ) + SW_LON
     p_lat = (points[1] / 111.323) + SW_LAT
     return [p_lon, p_lat]
@@ -102,31 +110,32 @@ def compute_line(x_coords, y_coords):
 def compute_nearest_point(new_point, slope, y_int): 
     x_nearest = (new_point[0] + slope*new_point[1] - y_int*slope) / (1 + slope**2)
     y_nearest = (new_point[0]*slope + slope**2*new_point[1] + y_int) / (1 + slope**2)
-    nearest_point = [x_nearest, y_nearest]
+    nearest_point = np.array([x_nearest, y_nearest])
     return nearest_point
 
-def compute_spree_prob(new_point, sigma_spree): 
+## Spree functions ## 
+
+def compute_spree_projected_pt(new_point, sigma_spree): 
     X_coords = SPREE_X
     Y_coords = SPREE_Y
     num_segs = len(SPREE_X) - 1
     point_distances = np.zeros(num_segs)
+    nearest_points = np.zeros(shape=(num_segs,2))
     # figure out which line segment the new_point is in:
     for i in xrange(num_segs):
-        seg_p1 = [X_coords[i], Y_coords[i]]
-        seg_p2 = [X_coords[i+1], Y_coords[i+1]]
+        seg_p1 = np.array([X_coords[i], Y_coords[i]])
+        seg_p2 = np.array([X_coords[i+1], Y_coords[i+1]])
         # compute line segment between current points:
         slope, y_int = compute_line([seg_p1[0], seg_p2[0]], [seg_p1[1], seg_p2[1]])
         # compute nearest point on that segment to our new point
         nearest_point = compute_nearest_point(new_point, slope, y_int)
         # make sure this point isn't outside the segment
-        # XXX if new_point[0] <= nearest_point[0]:
         if nearest_point[0] < seg_p1[0] and nearest_point[0] < seg_p2[0]:
             # check which x_coord is smaller. p1's or p2's, and make nearest pt that coord
             if seg_p1[0] < seg_p2[0]:
                 nearest_point = seg_p1
             else:
                 nearest_point = seg_p2
-        # XXX elif new_point[0] => nearest_point[0]:
         elif nearest_point[0] > seg_p1[0] and nearest_point[0] > seg_p2[0]:
             if seg_p1[0] > seg_p2[0]:
                 nearest_point = seg_p1
@@ -134,61 +143,121 @@ def compute_spree_prob(new_point, sigma_spree):
                 nearest_point = seg_p2
         # collect distance between this nearest point and our new point
         point_distances[i] = np.linalg.norm(new_point - nearest_point)
-    # find the nearest point of all nearest_points in all segments
+        # save the nearest point to the list, for later use 
+        nearest_points[i,:] = np.array(nearest_point).copy()  
+    # find the index of the nearest point of all nearest_points in all segments
     idx_nearest_point = point_distances.argmin()    
-    # compute the probability of the distance to this point
-    prob_of_new_point = log_gauss_pdf(point_distances[idx_nearest_point], 0, sigma_spree)
+    return nearest_points[idx_nearest_point]
+
+def compute_spree_prob(new_point, sigma_spree): 
+    nearest_point = compute_spree_projected_pt(new_point, sigma_spree)
+    dist = np.linalg.norm(new_point - nearest_point)
+    prob_of_new_point = log_gauss_pdf(dist, 0, sigma_spree)
     return prob_of_new_point
 
+## Gate functions ##
 
 def loglognorm(x, mu, sigma):
     log_prob = -np.log(sigma * np.sqrt(2*math.pi)) - np.log(x) - ( (np.log(x) - mu)**2 / (2*sigma**2) )
     return log_prob
 
 def compute_gate_prob(new_point, gate_point, sigma_gate, mean_gate):  
-    #log_norm = lognorm([sigma_gata],loc=MODE_GATE) # std, loc=mean
-
     dist = np.linalg.norm(new_point - gate_point)
-    #prob_of_new_point = lognorm.pdf(dist, np.exp(np.log(sigma_gate)) ) 
     log_prob = loglognorm(dist, mean_gate, sigma_gate)
     return log_prob
 
-def wiktors_butt(new_point, sigma_spree):
-    X_coords = SPREE_X
-    Y_coords = SPREE_Y
-    nn_dist = float('inf')
-    nn = np.array([0., 0.])
-    for k in range(1, len(X_coords)):
-        # 'NEW SEGMENT'
-        p1 = np.array([X_coords[k-1], Y_coords[k-1]])
-        p2 = np.array([X_coords[k], Y_coords[k]])
-        m, y0 = compute_line(np.array([p1[0], p2[0]]), np.array([p1[1], p2[1]]))
-        # print 'new_point0:',new_point
-        pp = np.array(compute_nearest_point(new_point, m, y0))
-        # print 'new_point1:',new_point
-        if ((pp[0] < p1[0] and pp[0] < p2[0]) or (pp[0] > p1[0] and pp[0] > p2[0])):
-            p1_dist = np.linalg.norm(p1 - new_point)
-            p2_dist = np.linalg.norm(p2 - new_point)
-            if p1_dist < p2_dist:
-                pp = p1
-            else:
-                pp = p2
-        np_dist = np.linalg.norm(pp - new_point)
-        if np_dist < nn_dist:
-            nn = np.array(pp)
-            nn_dist = np_dist
-    prob_of_new_point = gauss_pdf(nn_dist, 0.0, sigma_spree)
-    return prob_of_new_point
+
+## Sattelite functions ##
+
+def compute_satt_projected_pt(new_point, sigma_satt): 
+    slope, y_int = compute_line(SATT_X, SATT_Y)
+    nearest_point = compute_nearest_point(new_point, slope, y_int)
+    return nearest_point
 
 def compute_satt_prob(new_point, sigma_satt): 
     mean_satt = 0.
-    slope, y_int = compute_line(SATT_X, SATT_Y)
-    nearest_point = compute_nearest_point(new_point, slope, y_int)
+    nearest_point = compute_satt_projected_pt(new_point, sigma_satt)
     dist = np.linalg.norm(new_point - nearest_point) 
     prob_of_new_point = log_gauss_pdf(dist, mean_satt, sigma_satt)
     return prob_of_new_point
 
+def gauss_gradient(x_point, mu, sigma):
+    """
+    Spree and Sattelite
+    Compute gradient of a log Gaussian probability -- dL(lnN(x_{spr or sa}))/dx_{spr or sa} -- to use in objective function
+    """
+    grad = (x_point - mu) / (sigma**2+ .01**5)
+    return grad
+
+def lognorm_gradient(x_point, mu, sigma):
+    """
+    Gate:
+    Compute gradient of a log log-Normal probability -- dL(lnN(x_{g}))/dx_{g} -- to use in objective function
+    """
+    grad = - (1./x_point) -  ((np.log(x_point) - mu) / (x_point*sigma**2))
+    return grad
+
+def point_grad(eval_pt, nearest_pt):
+    """
+    Compute dx_{g,spr,or sa}/dq_x and dx_{g,spr,or sa}/dq_y to use in objective function
+    """
+    grad_x = eval_pt[0] - nearest_pt[0] / (np.linalg.norm(eval_pt - nearest_pt) + .01**10)
+    grad_y = eval_pt[1] - nearest_pt[1] / (np.linalg.norm(eval_pt - nearest_pt) + .01**10)
+    return np.array([grad_x, grad_y])
+
+def compute_obj_func(eval_pt):
+    """
+    Gradient of objective function: log joint probability, want to do coordinate ascent on to find max of
+    """
+    # compute the parameters of the three distributions
+    eval_pt = np.array(eval_pt)
+    
+    sigma_spree = compute_gauss_sigma(SIG2_SPREE, CONFIDENCE)
+    sigma_satt = compute_gauss_sigma(SIG2_SATT, CONFIDENCE)
+    sigma_gate, mu_gate = compute_logn_params(MEAN_GATE, MODE_GATE)
+    mu_spree = 0.
+    mu_satt = 0.
+    gate_pt = [GATE_X, GATE_Y]
+
+    # Spree:
+    spree_nearest_pt = compute_spree_projected_pt(eval_pt, sigma_spree)
+    spree_grad = gauss_gradient(spree_nearest_pt, sigma_spree, mu_spree) * point_grad(eval_pt, spree_nearest_pt)
+    # Satt:
+    satt_nearest_pt = compute_satt_projected_pt(eval_pt, sigma_satt)
+    satt_grad  = gauss_gradient(satt_nearest_pt, sigma_satt, mu_satt) * point_grad(eval_pt, satt_nearest_pt)
+    # Gate:
+    gate_dist = np.linalg.norm(eval_pt - gate_pt)
+    gate_grad  = lognorm_gradient(gate_dist, mu_gate, sigma_gate) * point_grad(eval_pt, gate_pt)
+    # Joint:
+    print "gradient components. spree: ", spree_grad,  "satt:", satt_grad, "gate:", gate_grad
+    joint_grad = spree_grad + satt_grad + gate_grad    
+    print 'joint_gradient:', joint_grad
+    return -joint_grad
+
+def compute_grad_ass():
+    """
+    Solve the problem by gradient ascent on the objective function, (-$, but no visualization)
+    """
+    # random init
+    init_pt =  np.array([6., 5.])
+    random_x = np.random.random_sample()*(X_MAX-X_MIN) + X_MIN
+    random_y = np.random.random_sample()*(Y_MAX-Y_MIN) + Y_MIN
+    init_pt = np.array([random_x, random_y]) 
+    print "init_pt", init_pt
+    # Optimize the shit out of this
+    # max_pt = compute_obj_func(init_pt)
+    # print 'max_pt:', max_pt
+    # max_pt = optimize.minimize(compute_obj_func, init_pt)
+    # FIXME TODO -- this part isn't done yet, need t ogive f(x) AND f'(x) as args
+    #max_pt = optimize.fmin_cg(compute_obj_func, init_pt)
+    max_pt = init_pt
+    return max_pt
+
 def compute_probs(RES):
+    """
+    Solve the problem using discretezation of the Region of Interest into a 2D grid,
+    computing the probability of the candidate at every point ($$$, but gives nice visualization!)
+    """
     # region of Berlin for which we care to calculate the probability of candidate
     X = np.linspace(X_MIN, X_MAX, RES)
     Y = np.linspace(Y_MIN, Y_MAX, RES)
